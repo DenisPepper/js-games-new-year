511c70b4ff8d3a0e5eaecc911fad401d28ab8d62
DenisPepper <thech@yandex.ru>
2023-12-12 00:25:46 +0300

    Зациклит задний фон

--- background.js
+++ background.js
@@ -7,17 +7,18 @@ export class Background {
     this.height = 893;
     this.x = 0;
     this.y = 0;
-    this.speed = 2;
+    this.speed = 1;
   }
 
   draw(context) {
     //context.fillStyle = 'white';
     //context.fillRect(this.x, this.y, this.width, this.height);
     context.drawImage(this.image, this.x, this.y);
+    context.drawImage(this.image, this.x + this.width, this.y);
   }
 
   update() {
     this.x -= this.speed;
-    if (this.x < 0 - this.width + this.playgroundWidth) this.x = 0;
+    if (this.x < 0 - this.width) this.x = 0;
   }
 }
